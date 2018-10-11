7e7a023d4f096bc9bf4eab4bcd0c4babd493b126
Tammo Jan Dijkema <dijkema@astron.nl>
2018-10-11 15:57:04 +0200

    Remove tabs

--- getdata_alta.py
+++ getdata_alta.py
@@ -29,43 +29,43 @@ def main(date, beams, task_ids, alta_exception):
 
     for beam_nr in beams:
     
-    	print("###########################")
-    	
-    	print('Processing Beam %.3d...' % beam_nr)
+        print("###########################")
+        
+        print('Processing Beam %.3d...' % beam_nr)
     
         for task_id in task_ids: 
-    		print('Processing task ID %.3d...' % task_id)
+            print('Processing task ID %.3d...' % task_id)
     
-    		if int(date) < 180216:
-    			cmd = "iget -rfPIT -X WSRTA%s%.2d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.2d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/home/apertif_main/wcudata/WSRTA%s%.2d/WSRTA%s%.2d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
-    		elif int(date) < 181003 or alta_exception == 'Y':
-    			cmd = "iget -rfPIT -X WSRTA%s%.3d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.3d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/home/apertif_main/wcudata/WSRTA%s%.3d/WSRTA%s%.3d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
-    		else:
-    			cmd = "iget -rfPIT -X WSRTA%s%.3d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.3d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/archive/apertif_main/visibilities_default/%s%.3d/WSRTA%s%.3d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
-    		print(cmd)
-    		os.system(cmd)
+            if int(date) < 180216:
+                cmd = "iget -rfPIT -X WSRTA%s%.2d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.2d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/home/apertif_main/wcudata/WSRTA%s%.2d/WSRTA%s%.2d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
+            elif int(date) < 181003 or alta_exception == 'Y':
+                cmd = "iget -rfPIT -X WSRTA%s%.3d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.3d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/home/apertif_main/wcudata/WSRTA%s%.3d/WSRTA%s%.3d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
+            else:
+                cmd = "iget -rfPIT -X WSRTA%s%.3d_B%.3d-icat.irods-status --lfrestart WSRTA%s%.3d_B%.3d-icat.lf-irods-status --retries 5 /altaZone/archive/apertif_main/visibilities_default/%s%.3d/WSRTA%s%.3d_B%.3d.MS" % (date,task_id,beam_nr,date,task_id,beam_nr,date,task_id,date,task_id,beam_nr)
+            print(cmd)
+            os.system(cmd)
     
     os.system('rm -rf *irods-status')
     
     # Add verification at the end of the transfer 
     for beam_nr in range(bstart,bend+1):
     
-    	print("###########################")
-    	
-    	print('Verifying Beam %.3d...' % beam_nr)
+        print("###########################")
+        
+        print('Verifying Beam %.3d...' % beam_nr)
     
-    	for task_id in range(int(istart),int(iend)+1):
-    		print('Processing task ID %.3d...' % task_id)
+        for task_id in range(int(istart),int(iend)+1):
+            print('Processing task ID %.3d...' % task_id)
     
-    		# Toggle for when we started using more digits:
-    		if int(date) < 180216:
-    			cmd = "irsync -srl i:/altaZone/home/apertif_main/wcudata/WSRTA%s%.2d/WSRTA%s%.2d_B%.3d.MS WSRTA%s%.2d_B%.3d.MS >> transfer_WSRTA%s%.2d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
-    		elif int(date) < 181003 or alta_exception == 'Y':
-    			cmd = "irsync -srl i:/altaZone/home/apertif_main/wcudata/WSRTA%s%.3d/WSRTA%s%.3d_B%.3d.MS WSRTA%s%.3d_B%.3d.MS >> transfer_WSRTA%s%.3d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
-    		else:
-    			cmd = "irsync -srl i:/altaZone/archive/apertif_main/visibilities_default/%s%.3d/WSRTA%s%.3d_B%.3d.MS WSRTA%s%.3d_B%.3d.MS >> transfer_WSRTA%s%.3d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
-    		
-    		os.system(cmd)
+            # Toggle for when we started using more digits:
+            if int(date) < 180216:
+                cmd = "irsync -srl i:/altaZone/home/apertif_main/wcudata/WSRTA%s%.2d/WSRTA%s%.2d_B%.3d.MS WSRTA%s%.2d_B%.3d.MS >> transfer_WSRTA%s%.2d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
+            elif int(date) < 181003 or alta_exception == 'Y':
+                cmd = "irsync -srl i:/altaZone/home/apertif_main/wcudata/WSRTA%s%.3d/WSRTA%s%.3d_B%.3d.MS WSRTA%s%.3d_B%.3d.MS >> transfer_WSRTA%s%.3d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
+            else:
+                cmd = "irsync -srl i:/altaZone/archive/apertif_main/visibilities_default/%s%.3d/WSRTA%s%.3d_B%.3d.MS WSRTA%s%.3d_B%.3d.MS >> transfer_WSRTA%s%.3d_to_alta_verify.log 2>&1" % (date,task_id,date,task_id,beam_nr,date,task_id,beam_nr,date,task_id)
+            
+            os.system(cmd)
     
     # Identify server details
     hostname = os.popen('hostname').read().strip()
@@ -73,20 +73,20 @@ def main(date, beams, task_ids, alta_exception):
     
     # Check for failed files
     for task_id in range(int(istart),int(iend)+1):
-    	print('Processing task ID %.3d...' % task_id)
-    	
-    	cmd = os.popen('cat transfer_WSRTA%s%.3d_to_alta_verify.log | wc -l' % (date,task_id))
-    	for x in cmd:
-    		print('Failed files:',x.strip())
-    		failed_files = x.strip()
+        print('Processing task ID %.3d...' % task_id)
+        
+        cmd = os.popen('cat transfer_WSRTA%s%.3d_to_alta_verify.log | wc -l' % (date,task_id))
+        for x in cmd:
+            print('Failed files:',x.strip())
+            failed_files = x.strip()
     
-    	if failed_files == '0':
-    		cmd = """curl -X POST --data-urlencode 'payload={"text":"Transfer of WSRTA%s%.3d (B%.3d-B%.3d) from ALTA to %s finished."}' https://hooks.slack.com/services/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""" % (date,task_id,bstart,bend,hostname)
-    	else:
-    		cmd = """curl -X POST --data-urlencode 'payload={"text":"Transfer of WSRTA%s%.3d (B%.3d-B%.3d) from ALTA to %s finished incomplete. Check logs!"}' https://hooks.slack.com/services/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""" % (date,task_id,bstart,bend,hostname)
+        if failed_files == '0':
+            cmd = """curl -X POST --data-urlencode 'payload={"text":"Transfer of WSRTA%s%.3d (B%.3d-B%.3d) from ALTA to %s finished."}' https://hooks.slack.com/services/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""" % (date,task_id,bstart,bend,hostname)
+        else:
+            cmd = """curl -X POST --data-urlencode 'payload={"text":"Transfer of WSRTA%s%.3d (B%.3d-B%.3d) from ALTA to %s finished incomplete. Check logs!"}' https://hooks.slack.com/services/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""" % (date,task_id,bstart,bend,hostname)
     
-    	# Execute the command
-    	os.system(cmd)
+        # Execute the command
+        os.system(cmd)
     
     
     print("###########################")
@@ -103,30 +103,30 @@ if __name__ == "__main__":
     args = sys.argv
     # Get date
     try:
-    	date = args[1]
+        date = args[1]
     except:
-    	print("Date required! Format: YYMMDD e.g. 180309")
-    	sys.exit()
+        print("Date required! Format: YYMMDD e.g. 180309")
+        sys.exit()
     
     # Get date
     try:
-    	irange = args[2]
+        irange = args[2]
     except:
-    	print("ID range required! Format: NNN-NNN e.g. 002-010")
-    	sys.exit()
+        print("ID range required! Format: NNN-NNN e.g. 002-010")
+        sys.exit()
     
     # Get beams
     try: 
-    	brange = args[3]
+        brange = args[3]
     except:
-    	print("Beam range required! Format: NN-NN e.g. 00-37")
-    	sys.exit()
+        print("Beam range required! Format: NN-NN e.g. 00-37")
+        sys.exit()
     
     # Get beams
     try: 
-    	alta_exception = args[4]
+        alta_exception = args[4]
     except:
-    	alta_exception = 'N'
+        alta_exception = 'N'
     
     # Now with all the information required, loop through beams
     bstart = int(brange.split('-')[0])
