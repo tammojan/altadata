fcb03ed6fab4b7e094adc52f6f589fd648660bcc
Vanessa Moss <moss@nbruiter-w10.nfra.nl>
2018-10-12 21:49:50 +0200

    Rename header

--- usage_alta.py
+++ usage_alta.py
@@ -36,7 +36,7 @@ def main():
 
 	# Return the output
 	print('\n##########################################################################################')
-	print('NODE\t\t\tSTATUS\t\t\FREE(TB)\t\t\tUSAGE(%)')
+	print('NODE\t\t\tSTATUS\t\t\tFREE(TB)\t\t\tUSAGE(%)')
 	print ''.join(lines).strip('\n')
 	print('##########################################################################################\n')
 
